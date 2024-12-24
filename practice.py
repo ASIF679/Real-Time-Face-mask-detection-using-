50ec195663ebf9ebda1557c77c58f737302733fe
Your Name <asifnawaz679@gmail.com>
2024-12-24 23:46:10 +0500

    first commit

--- practice.py
+++
@@ -1,9 +0,0 @@
-def sumNumber(numbers):
-    total = 0
-    for num in numbers:
-        total += num
-    return total
-
-# Example usage
-numbers_list = [1, 2, 3, 4, 5]  # Example list of numbers
-print(sumNumber(numbers_list))
